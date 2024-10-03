c2f202018f7ad3d260887bcbd4b22a0f00da1209
Felipe Camargo <felipe.c@outlook.com>
2024-10-02 22:40:32 -0300

    subida banco de dados

---
+++ src/main/java/com/fobctech/dscommerce/entities/Users.java
@@ -0,0 +1,90 @@
+package com.fobctech.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String names;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+    private List<String> roles;
+
+    public Users(){
+
+    }
+    public Users(long id, String names, String email, String phone, LocalDate birthDate, String password, List<String> roles) {
+        this.id = id;
+        this.names = names;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNames() {
+        return names;
+    }
+
+    public void setNames(String names) {
+        this.names = names;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
